a7e1529ab1a08cb884f920150b9fc32fb065c35b
Callux <calluxcarlyle@gmail.com>
2025-05-03 00:22:04 +0100

    Changed Express GET code to showcase Index.html file

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ let express = require('express');
 let app = express();
 
 app.get("/", function(req, res) { 
-    res.send('Hello Express');
+    res.sendFile(absolutePath = __dirname + '/views/index.html');
 });
+
+console.log("Hello World");
 
 
 
