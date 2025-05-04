65e46e1d56204f9a463874778d2360f6a3dab07b
Callux <calluxcarlyle@gmail.com>
2025-05-04 22:55:30 +0100

    Added Simple IP Logger

--- myApp.js
+++ myApp.js
@@ -10,6 +10,11 @@ app.use("/public", function(req, res) {
     res.sendFile(absolutePath = __dirname + '/public/style.css');
 });
 
+app.get("/json", function (req, res, next) {
+    console.log(req.method, req.path, req.ip);
+    next();
+});
+
 app.get("/json", function(req, res) {
     if (process.env.MESSAGE_STYLE == "uppercase") {
         res.json({"message": "HELLO JSON"});
